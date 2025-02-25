3b94ffbee16e56990202e0eea1bfc708d2815f85
ELL <ellipszist@gmail.com>
2025-02-25 14:13:08 +0700

    Update 2025 v1

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import icon from 'astro-icon';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://gamesbeyondborders.info',
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind({
+      applyBaseStyles: false
+    }),
+    icon()
+  ]
+});
---
+++ src/content/config.ts
@@ -0,0 +1,43 @@
+import { defineCollection, z } from 'astro:content';
+
+const seoSchema = z.object({
+  title: z.string().min(5).max(120).optional(),
+  description: z.string().min(15).max(160).optional(),
+  image: z
+    .object({
+      src: z.string(),
+      alt: z.string().optional()
+    })
+    .optional(),
+  pageType: z.enum(['website', 'article']).default('website')
+});
+
+const stories = defineCollection({
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      excerpt: z.string().optional(),
+      publishDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      // isFeatured: z.boolean().default(false),
+      tags: z.array(z.string()).default([]),
+      cover: image().refine((img) => img.width >= 1080, {
+        message: 'Cover image must be at least 1080 pixels wide!'
+      }),
+      facebookPost: z.string().optional(),
+      seo: seoSchema.optional()
+    })
+});
+
+const pages = defineCollection({
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      cover: image().refine((img) => img.width >= 1080, {
+        message: 'Cover image must be at least 1080 pixels wide!'
+      }),
+      seo: seoSchema.optional()
+    })
+});
+
+export const collections = { stories, pages };
